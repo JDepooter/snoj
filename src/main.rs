4ea1be23b6a8554571776b33c7548682b2b0d422
Joel Depooter <joel.depooter@safe.com>
2020-01-29 13:36:52 -0800

    Read input file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 #[macro_use]
 extern crate clap;
+
 use clap::App;
+use serde_json::Value;
+use std::fs::File;
+use std::io::BufReader;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+
+    let source_file = matches.value_of("input").unwrap();
+    println!("Reading JSON file: {}", source_file);
+    let file = match File::open(source_file) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Could not open input file!");
+            println!("{}", e);
+            return;
+        }
+    };
+
+    let reader = BufReader::new(file);
+    let json: Value = match serde_json::from_reader(reader) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Unable to parse source file!");
+            println!("{}", e);
+            return;
+        }
+    };
 }
